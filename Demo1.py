6cc735d84cc64be94231103498fa62af779cea3c
jyotics24 <jyotiprakashkhuntia999@gmail.com>
2024-11-02 20:25:16 +0530

    Add 1st string file

---
+++ Demo1.py
@@ -0,0 +1,12 @@
+# From-1
+# string[start:stop:step]
+# Wap to Display the string and reverse it
+# Given string is "Hello Python"
+import time 
+str="Hello Python"
+print()
+print("Before Reverse :",str)
+print()
+print("Afre Reverse :",str[::-1])
+time.sleep(2)
+print("End of an application")
