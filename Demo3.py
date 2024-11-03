fe4392e9a16d5304f86f1094ffb89bcced8c002f
jyotics24 <jyotiprakashkhuntia999@gmail.com>
2024-11-03 10:08:14 +0530

    Add the string Concart file

---
+++ Demo3.py
@@ -0,0 +1,9 @@
+# Wap to perfrom String concatenation,repetation and all arethimatic operator only using one variable.
+import time
+X1=eval(input("Enter the value of X1 :")) # eval Any type of data is support
+print(X1)
+print()
+print(type(X1))
+print()
+time.sleep(2)
+print("End of an application")
