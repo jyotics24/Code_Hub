66700d13aa77bd26a7f75fb43d4f4a53303719fb
jyotics24 <jyotiprakashkhuntia999@gmail.com>
2024-11-03 14:10:37 +0530

    Add Demo-4

---
+++ Demo4.py
@@ -0,0 +1,11 @@
+# Wap to add two list without using extend().
+l1=[101,102,103,104]
+l2=[ord("A"),ord("B"),ord("C"),ord("D")]
+print()
+print(l1)
+print()
+print(l2)
+l3=l1+l2
+print()
+print(l3)
+print("End")
