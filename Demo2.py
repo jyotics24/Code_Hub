a42bb09baa2d69583ba0d06ff3246440e46d4579
jyotics24 <jyotiprakashkhuntia999@gmail.com>
2024-11-03 09:06:20 +0530

    Add the file

---
+++ Demo2.py
@@ -0,0 +1,18 @@
+# wap to show example of TyepeCasting
+import time 
+print("Before TypeCasting")
+A1=input("Enter The Value Of A1 :")
+A2=input("Enter The Value Of A2 :")
+B=A1+A2
+print()
+print("Result Is :",B)
+print()
+print("After TypeCasting ")
+print()
+A1=int(input("Enter The Value Of A1 :"))
+A2=int(input("Enter The Value Of A2 :"))
+B=A1+A2
+print()
+print("Result Is :",B)
+time.sleep(2)
+print("End of an application")
